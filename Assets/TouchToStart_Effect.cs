524300b80e17022dc1d115a883c3888453837e9d
Roh Geonho <roh517@naver.com>
2019-09-21 22:36:51 +0900

    Menu Created
    fade in / out Loop effect created
    GameObjects created

---
+++ Assets/TouchToStart_Effect.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchToStart_Effect : MonoBehaviour
+{
+   
+    public Text txt;
+	
+	private float alpha = 0.0f;
+	private bool status;
+   
+    void Start()
+    {
+        txt = GetComponent<Text> ();
+		txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, alpha);
+		status = true;
+
+    	StartCoroutine ("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {	
+    	while(true)
+		{
+			if(status == true)
+			{
+				alpha += 0.01f;
+				txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, alpha);
+				if(alpha >= 1.0f)
+				{
+					status = false;
+				}
+				yield return new WaitForSeconds (0.01f);
+			}
+
+			if(status == false)
+			{
+				alpha -= 0.01f;
+				txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, alpha);
+				if(alpha <= 0.0f)
+				{
+					status = true;
+				}
+				yield return new WaitForSeconds (0.01f);
+			}
+			
+		}
+    }
+  
+}
+
