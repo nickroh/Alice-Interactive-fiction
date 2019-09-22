7a4d22fce63937e40c56e0224013bfa70a068f94
Roh Geonho <roh517@naver.com>
2019-09-22 19:58:38 +0900

    Touch  to Start (err)
    need to be fixed

---
+++ Assets/Start_Touch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems; 
+
+public class Start_Touch : MonoBehaviour
+{
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+		{
+			if(!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+			{  
+	         	change();
+			}
+		}
+
+        if(Input.GetMouseButtonDown(0)) 
+		{
+			if(!EventSystem.current.IsPointerOverGameObject())
+			{  
+	        	 change();
+			}
+	
+		}
+    }
+
+    void change()
+    {
+    	SceneManager.LoadScene("chp_1.1");
+    } 
+}
