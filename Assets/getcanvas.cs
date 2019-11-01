500890e3f5b01bdd56961b91bb6ea4610a3de92e
Roh Geonho <roh517@naver.com>
2019-11-02 00:21:52 +0900

    scenes to prefab
    load prefab

---
+++ Assets/getcanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getcanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {	
+        GameObject prefab = Resources.Load("Prefabs/Canvas") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
