6750f058addf69dbda1548e1ac4def6c49d47f37
Roh Geonho <roh517@naver.com>
2019-11-02 19:57:58 +0900

    scenes to prefab
    rendering order problem fixed

--- Assets/getcanvas.cs
+++ Assets/getcanvas.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class getcanvas : MonoBehaviour
 {
+	public GameObject Screen;
+	Vector3 pos;
+	Quaternion angle;
+
     // Start is called before the first frame update
     void Start()
     {	
-        GameObject prefab = Resources.Load("Prefabs/Canvas") as GameObject;
+    	LoadScreen();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void LoadScreen()
+    {
+    	pos = new Vector3(0,0,0);
+    	angle = Quaternion.identity;
+    	//GameObject screen = Resources.Load("Prefabs/Canvas") as GameObject;
+    	GameObject instance = Instantiate(Screen,pos,angle);
     }
 }
