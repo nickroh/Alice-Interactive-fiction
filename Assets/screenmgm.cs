3202a7cd5042a3f898075f3e4d95ad8012b1d4ea
Roh Geonho <roh517@naver.com>
2019-11-12 15:18:00 +0900

    ..

---
+++ Assets/screenmgm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenmgm : MonoBehaviour
+{
+	// public GameObject first;
+	// public GameObject second;
+	// public GameObject third;
+
+	Vector3 pos;
+	Quaternion angle;
+
+	void Start()
+	{
+
+	}
+
+    void first_btn()
+    {
+    	
+    }
+
+    void second_btn()
+    {
+    	
+   
+    }
+
+    void third_btn()
+	{
+		
+		
+    }
+
+    void Delete()
+    {
+    // 	Destroy(this.Gameobject);
+    }
+}
