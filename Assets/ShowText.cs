930dc5dca823b20aecba639714b26dfc7601187e
Roh Geonho <roh517@naver.com>
2019-11-27 16:24:17 +0900

    story_update

---
+++ Assets/ShowText.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShowText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadXml();	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
