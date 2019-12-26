36521d891b23b070c0096151ea075184ec57da4d
Roh Geonho <roh517@naver.com>
2019-12-27 04:02:43 +0900

    정리

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue: MonoBehaviour 
+{
+	public static Dialogue instance;
+
+	public ELEMENTS elements;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	/// <summary>
+	/// Say something and show it on the speech box.
+	/// </summary>
+	public void Say(string speech)
+	{
+		StopSpeaking();
+
+		speaking = StartCoroutine(Speaking(speech, false));
+	}
+
+	/// <summary>
+	/// Say something to be added to what is already on the speech box.
+	/// </summary>
+	public void SayAdd(string speech)
+	{
+		StopSpeaking();
+
+		speechText.text = targetSpeech;
+
+		speaking = StartCoroutine(Speaking(speech, true));
+	}
+
+	public void StopSpeaking()
+	{
+		if (isSpeaking)
+		{
+			StopCoroutine(speaking);
+		}
+		speaking = null;
+	}
+		
+	public bool isSpeaking {get{return speaking != null;}}
+	[HideInInspector] public bool isWaitingForUserInput = false;
+
+	public string targetSpeech = "";
+	Coroutine speaking = null;
+	IEnumerator Speaking(string speech, bool additive)
+	{
+		speechPanel.SetActive(true);
+		targetSpeech = speech;
+
+		if (!additive)
+			speechText.text = "";
+		else
+			targetSpeech = speechText.text + targetSpeech;
+
+		isWaitingForUserInput = false;
+
+		while(speechText.text != targetSpeech)
+		{
+			speechText.text += targetSpeech[speechText.text.Length];
+			yield return new WaitForEndOfFrame();
+		}
+
+		//text finished
+		isWaitingForUserInput = true;
+		while(isWaitingForUserInput)
+			yield return new WaitForEndOfFrame();
+
+		StopSpeaking();
+	}
+
+
+	[System.Serializable]
+	public class ELEMENTS
+	{
+		/// <summary>
+		/// The main panel containing all dialogue related elements on the UI
+		/// </summary>
+		public GameObject speechPanel;
+		public Text speechText;
+	}
+	public GameObject speechPanel {get{return elements.speechPanel;}}
+	public Text speechText {get{return elements.speechText;}}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class Test : MonoBehaviour
+{
+	Dialogue dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = Dialogue.instance;
+    }
+
+    public string[] s = new string[]
+    {
+    	"test sentence 1",
+    	"test sentence 2",
+    	"test sentence 3"
+    };
+
+    int index = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+		{
+			if(!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+			{  
+	         	if(!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+	         	{
+	         		if(index >= s.Length)
+	         		{
+	         			return;
+	         		}
+	         		Say(s[index]);
+	         		index++;
+	         	}
+			}
+		}
+
+        else if(Input.GetMouseButtonDown(0)) 
+		{
+		   	 if(!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+	         	{
+	         		if(index >= s.Length)
+	         		{
+	         			return;
+	         		}
+	         		Say(s[index]);
+	         		index++;
+	         	}
+		}
+
+		void Say(string s)
+		{
+			dialogue.Say(s);
+		}
+    }
+}
