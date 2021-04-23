883185fd23c2a093c555fcd880ae07b78ebb515c
nickroh <48642955+nickroh@users.noreply.github.com>
2021-04-23 18:29:54 +0900

    hp_bar

--- Assets/hp_view.cs
+++ Assets/hp_view.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hp_view : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public Slider HP_bar;
+     
+
+    void start(){
+        HP_bar.value = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(HP_bar.value<=0){
+            
+        }
     }
+
+    public void Show(){
+        gameObject.SetActive(true);
+    }
+
+    public void Hide(){
+        gameObject.SetActive(false);
+    }
+
+
 }
